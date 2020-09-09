7ace0054011cad0eb6bb8c28b0749f4eeff2cc26
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-09-08 23:44:25 -0300

    Aula 42 Finalizada

---
+++ Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aprendendo_c_sharp_1 {
+    class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+    }
+}
